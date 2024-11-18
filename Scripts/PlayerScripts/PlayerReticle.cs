76df784417d1bbe7191ec9c204cbf0c79b242e46
Jonathan <122506441+jwl0724@users.noreply.github.com>
2024-11-18 12:01:43 -0800

    implement camera reticle

--- Scripts/PlayerScripts/PlayerReticle.cs
+++ Scripts/PlayerScripts/PlayerReticle.cs
@@ -2,11 +2,34 @@ using Godot;
 using System;
 
 public partial class PlayerReticle : RayCast3D {
+	private Player player;
+	private HitBox lookAtTarget;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
+		player = Owner as Player;
+		player.Connect(Player.SignalName.PlayerInteract, Callable.From(() => OnPlayerInteract()));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
+	}
+
+    public override void _PhysicsProcess(double delta) {
+        if (!IsColliding()) {
+			lookAtTarget?.TriggerLookedAway();
+			lookAtTarget = null;
+			return;
+		}
+		HitBox target = GetCollider() as HitBox;
+		if (target == lookAtTarget) return;
+		else {
+			lookAtTarget = target;
+			lookAtTarget.TriggerLookedAt();
+		}
+    }
+
+    private void OnPlayerInteract() {
+		lookAtTarget?.GetInteractableEntity()?.Interact();
 	}
 }
