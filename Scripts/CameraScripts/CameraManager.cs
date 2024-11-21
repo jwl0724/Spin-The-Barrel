8c243c0aac13e2d4a9972af2e3a54e0639b3fb5d
Jonathan <122506441+jwl0724@users.noreply.github.com>
2024-11-21 13:13:42 -0800

    add implementation of switching cameras

--- Scripts/CameraScripts/CameraManager.cs
+++ Scripts/CameraScripts/CameraManager.cs
@@ -10,29 +10,49 @@ public partial class CameraManager : Node {
 		POST_GAME_CAMERA,
 		PLAYER_CAMERA
 	}
-
 	private Camera3D backgroundCamera;
 	private Camera3D deadCamera;
 	private Camera3D lobbyCamera;
 	private Camera3D postGameCamera;
 	private Camera3D playerCamera = null;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() {
+	CameraManager() {
 		Instance = this;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+		// applicable to any scene as long as cameras are named accordingly
+		backgroundCamera = GetNode<Camera3D>("BackgroundCamera");
+		deadCamera = GetNode<Camera3D>("DeadCamera");
+		lobbyCamera = GetNode<Camera3D>("LobbyCamera");
+		postGameCamera = GetNode<Camera3D>("PostGameCamera");
+
+		GameManager.Instance.Connect(GameManager.SignalName.GameStateChanged, Callable.From((GameManager.GameState state) => OnGameStateChange(state)));
 	}
 
-	public void AddPlayerCamera(Camera3D camera) {
+	public void SwitchToPlayerCamera(Camera3D camera) {
 		playerCamera = camera;
+		playerCamera.Current = true;		
 	}
 
-	public static void SetCurrentCamera(Camera camera) {
-
+	public void SwitchToDeadCamera() {
+		deadCamera.Current = true;
 	}
 
-	
+	private void OnGameStateChange(GameManager.GameState state) {
+		switch (state) {
+			case GameManager.GameState.MAIN_MENU:
+				backgroundCamera.Current = true;
+				break;
+			case GameManager.GameState.LOBBY:
+				lobbyCamera.Current = true;
+				break;
+			case GameManager.GameState.POST_GAME:
+				playerCamera.Current = false;
+				playerCamera = null;
+				postGameCamera.Current = true;
+				break;
+		}
+	}
 }
