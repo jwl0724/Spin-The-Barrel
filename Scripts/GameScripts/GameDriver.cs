045bafc9e3acb067f94a21fc460e7de1bd467c0c
Jonathan <122506441+jwl0724@users.noreply.github.com>
2024-11-21 13:14:55 -0800

    implement singleton for driver

--- Scripts/GameScripts/GameDriver.cs
+++ Scripts/GameScripts/GameDriver.cs
@@ -3,6 +3,12 @@ using System;
 using System.Collections.Generic;
 
 public partial class GameDriver : Node {
+	// SET UP SINGLETON
+	public static GameDriver Instance { get; private set; }
+	GameDriver() {
+		Instance = this;
+	}
+
 	// EXPORT VARIABLES
 	[Export] private PackedScene playerScene; // scene for player
 	[Export] private PackedScene remotePlayerScene; // scene for remote player
@@ -12,13 +18,14 @@ public partial class GameDriver : Node {
 	
 
 	// STATIC CONSTANTS
-	public static readonly List<Player> Players = new List<Player>(4); // list of players in the game
+	public static readonly List<Player> Players = new(4); // list of players in the game
 
 	// VARIABLES
 	public Player currentTurnPlayer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,11 +39,13 @@ public partial class GameDriver : Node {
 			Node3D spawnPoint = spawnPoints.GetChild(i) as Node3D;
 			if (i == randomPlayerPosition) {
 				Player player = playerScene.Instantiate<Player>();
+				player.SetModel(player.SelectedModel);
 				Players.Add(player);
 				spawnPoint.AddChild(player);
 
 			} else {
 				Player remotePlayer = remotePlayerScene.Instantiate<Player>();
+				remotePlayer.SetModel(remotePlayer.SelectedModel);
 				Players.Add(remotePlayer);
 				spawnPoint.AddChild(remotePlayer);
 			}
