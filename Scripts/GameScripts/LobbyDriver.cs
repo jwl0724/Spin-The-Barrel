4e3f7dd783ec8b13b3388f892c99721becc7ae1f
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-22 05:05:04 -0800

    implement add and remove players

--- Scripts/GameScripts/LobbyDriver.cs
+++ Scripts/GameScripts/LobbyDriver.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class LobbyDriver : Node3D {
 	// SINGLETON SETUP	
@@ -13,7 +14,9 @@ public partial class LobbyDriver : Node3D {
 	[Signal] public delegate void PlayerLeaveLobbyEventHandler();
 
 	// VARIABLES
-	public int PlayerCount { get; private set; } = 1;
+	public static readonly int MAX_PLAYERS = 4;
+	public static readonly List<PlayerInfo> Players;
+	public int LocalPlayerIndex { get; private set; } = -1;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
@@ -22,12 +25,25 @@ public partial class LobbyDriver : Node3D {
 		);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
+	public void RemovePlayer(PlayerInfo info) {
+		Players.Remove(info);
+		EmitSignal(SignalName.PlayerLeaveLobby);
+	}
+
+	public void AddPlayer(PlayerInfo info) {
+		if (Players.Count >= MAX_PLAYERS) {
+			GD.PushWarning("Attempting to add beyond max players");
+			return;
+		}
+		if (info.IsRemote) Players.Add(info);
+		else if (LocalPlayerIndex == -1) LocalPlayerIndex = Players.Count;
+		else GD.PushError("Trying to create multiple local players");
+		EmitSignal(SignalName.PlayerJoinLobby);
 	}
 
 	private void OnGameStateChange(GameManager.GameState state) {
 		// TODO: Add some more logic here if needed
 		Visible = state == GameManager.GameState.LOBBY;
+		LocalPlayerIndex = -1;
 	}
 }
