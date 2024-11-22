4121eedea11364be92d7b18d2c479457d0a3ff85
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-22 05:06:16 -0800

    create new data class for players in lobby

---
+++ Scripts/GameScripts/PlayerInfo.cs
@@ -0,0 +1,6 @@
+public class PlayerInfo {
+	// TODO: Add more stuff here later that needs to be tracked in lobby	
+	public bool IsRemote = false;
+	public string Name = "";
+	// probably socket id here when networking is done
+}
