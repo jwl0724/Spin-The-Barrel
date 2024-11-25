dd44e140bc7553946d2600776680ae64bab0ad9e
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-25 03:00:15 -0800

    create fade in effect for all element components

--- Scripts/MenuScripts/MainMenu.cs
+++ Scripts/MenuScripts/MainMenu.cs
@@ -6,18 +6,23 @@ public partial class MainMenu : Control, IMenu {
 	private static readonly string BLUR_RECT_NODE_NAME = "Blur";
 	private static readonly string BLUR_SHADER_PARAM = "amount";
 	private ShaderMaterial blurEffect;
+	private Godot.Collections.Array<Control> components = new();
 
 	// CONSTANTS
 	private const float finalBlurAmount = 1f;
+	private const float fadeInTime = 2.5f;
+	private readonly Color opaqueColor = new(1, 1, 1, 1);
+	private readonly Color transparentColor = new(1, 1, 1, 0);
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		Material blurMaterial = (GetNode(BLUR_RECT_NODE_NAME) as Control).Material;
 		blurEffect = blurMaterial as ShaderMaterial;
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
+		foreach(Control component in GetChildren()) {
+			if (component.Name == BLUR_RECT_NODE_NAME) continue;
+			component.Modulate = transparentColor;
+			components.Add(component);
+		}
 	}
 
     public void ShowScreen() {
@@ -25,6 +30,7 @@ public partial class MainMenu : Control, IMenu {
 		Visible = true;
         SetProcessInput(true);
 		BlurBackground(true);
+		foreach(var component in components) FadeIn(component, fadeInTime);
     }
 
     public void HideScreen() {
@@ -32,11 +38,16 @@ public partial class MainMenu : Control, IMenu {
 		Visible = false;
 		SetProcessInput(false);
 		BlurBackground(false);
+		foreach(var component in components) FadeOut(component, fadeInTime);
     }
 
     public string GetName() {
         return this.Name;
     }
+
+	public void GoNextScreen(ScreenManager.ScreenState newState) {
+		(GetParent() as MenuManager).NotifyChange(newState);
+	}
 
 	private void BlurBackground(bool doBlur) {
 		if (!doBlur) {
@@ -45,10 +56,20 @@ public partial class MainMenu : Control, IMenu {
 		} 
 		Tween blurTween = CreateTween();
 		blurTween.TweenMethod(Callable.From((float amount) => {
-			GD.Print(amount);
 			blurEffect.SetShaderParameter(BLUR_SHADER_PARAM, amount);
-		}), 0f, finalBlurAmount, 2.5f);
+		}), 0f, finalBlurAmount, fadeInTime);
 		blurTween.Play();
 	}
-	
+
+	private void FadeIn(Control node, float time) {
+		Tween fadeTween = CreateTween();
+		fadeTween.TweenProperty(node, nameof(Modulate).ToLower(), opaqueColor, time);
+		fadeTween.Play();
+	}
+
+	private void FadeOut(Control node, float time) {
+		Tween fadeTween = CreateTween();
+		fadeTween.TweenProperty(node, nameof(Modulate).ToLower(), transparentColor, time);
+		fadeTween.Play();
+	}
 }
