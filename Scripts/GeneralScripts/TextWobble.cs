453a8b517becccb68d4a423e4a7c08da06819ba5
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-25 02:58:52 -0800

    add export tag to variables

--- Scripts/GeneralScripts/TextWobble.cs
+++ Scripts/GeneralScripts/TextWobble.cs
@@ -2,19 +2,16 @@ using Godot;
 using System;
 
 public partial class TextWobble : Label {
-	private float scaleOffset = 0.1f;
-	private float scaleSpeed = 2f;
-	private float rotationOffset = 10f;
-	private float rotationSpeed = 3f;
+	[Export] private float scaleOffset = 0.1f;
+	[Export] private float scaleSpeed = 2f;
+	[Export] private float rotationOffset = 10f;
+	[Export] private float rotationSpeed = 3f;
 	private float angle = 0;
 
 	public override void _Process(double delta) {
 		if (angle >= 2 * Mathf.Pi) angle = 0;
 		Scale += Vector2.One * scaleOffset * Mathf.Sin(angle * scaleSpeed) * (float) delta;
 		Rotation = rotationOffset * Mathf.Sin(angle * rotationSpeed) * (float) delta;
-		GD.Print(angle);
-		GD.Print(delta);
-		GD.Print("--------------------");
 		angle += (float) delta;
 	}
 }
