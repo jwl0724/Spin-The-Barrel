4beb9e89c2aaf6bffc0db2d3bd3b18a0616dba89
Juntingg <jsu62@my.bcit.ca>
2024-11-25 17:44:36 -0800

    FIX: remove delta dependency

--- Scripts/GeneralScripts/TextWobble.cs
+++ Scripts/GeneralScripts/TextWobble.cs
@@ -1,17 +1,33 @@
 using Godot;
 using System;
 
-public partial class TextWobble : Label {
+public partial class TextWobble : Label
+{
 	[Export] private float scaleOffset = 0.1f;
 	[Export] private float scaleSpeed = 2f;
 	[Export] private float rotationOffset = 10f;
 	[Export] private float rotationSpeed = 3f;
 	private float angle = 0;
+	private double startTime;
 
-	public override void _Process(double delta) {
-		if (angle >= 2 * Mathf.Pi) angle = 0;
-		Scale += Vector2.One * scaleOffset * Mathf.Sin(angle * scaleSpeed) * (float) delta;
-		Rotation = rotationOffset * Mathf.Sin(angle * rotationSpeed) * (float) delta;
-		angle += (float) delta;
+	public override void _Ready()
+	{
+		startTime = EngineTimeSeconds();
 	}
+
+	public override void _Process(double delta)
+	{
+		double elapsedTime = EngineTimeSeconds() - startTime;
+		float scaleFactor = scaleOffset * Mathf.Sin((float)(elapsedTime * scaleSpeed));
+		Scale = Vector2.One * (1 + scaleFactor);
+
+		float rotationAngle = Mathf.DegToRad(rotationOffset) * Mathf.Sin((float)(elapsedTime * rotationSpeed));
+		Rotation = rotationAngle;
+	}
+
+	private double EngineTimeSeconds()
+	{
+		return Time.GetTicksMsec() / 1000.0;
+	}
+
 }
