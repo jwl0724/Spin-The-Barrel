dc3132c5f19a0b2383cd59f0b79178280641816d
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-26 06:26:59 -0800

    create script to handle getting lobby code

---
+++ Scripts/MenuScripts/JoinLobbyInput.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class JoinLobbyInput : Control {
+	private static readonly string LINE_EDIT_NODE_NAME = "Input";
+	private static readonly string SUBMIT_BUTTON_NODE_NAME = "Submit";
+	private JoinLobbyMenu joinLobbyMenu;
+	private LineEdit inputField;
+
+	public override void _Ready() {
+		joinLobbyMenu = Owner as JoinLobbyMenu;
+		inputField = GetNode<LineEdit>(LINE_EDIT_NODE_NAME);
+		inputField.Connect(LineEdit.SignalName.TextSubmitted, Callable.From((string code) => OnSubmit(code)));
+		GetNode<Button>(SUBMIT_BUTTON_NODE_NAME).Connect(Button.SignalName.Pressed, Callable.From(
+			() => OnSubmitButtonPressed()
+		));
+	}
+
+	private void OnSubmit(string lobbyCode) {
+		joinLobbyMenu.SubmitLobbyCode(lobbyCode);
+	}
+
+	private void OnSubmitButtonPressed() {
+		string lobbyCode = inputField.Text;
+		OnSubmit(lobbyCode);
+	}
+}
