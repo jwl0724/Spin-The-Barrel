e717564f67caa1ebf5b6bbbbbb3593663106b889
Juntingg <jsu62@my.bcit.ca>
2024-11-25 17:03:09 -0800

    FIX: preload character not rotate

--- Scripts/GeneralScripts/PreviewEffect.cs
+++ Scripts/GeneralScripts/PreviewEffect.cs
@@ -1,25 +1,32 @@
 using Godot;
 using System;
 
-public partial class PreviewEffect : Node3D {
+public partial class PreviewEffect : Node3D
+{
 	private float angleVariable = 0;
+	private float rotationAngle = 0;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
+	public override void _Process(double delta)
+	{
 		DoHoverEffect(delta);
 		DoRotateEffect(delta);
 	}
 
-	private void DoHoverEffect(double delta) {
+	private void DoHoverEffect(double delta)
+	{
 		const int offsetSpeed = 8;
 		float offsetAmount = 0.005f;
 		if (angleVariable >= Mathf.Pi * 2) angleVariable = 0;
-		angleVariable += (float) delta * offsetSpeed;
+		angleVariable += (float)delta * offsetSpeed;
 		Position += Vector3.Up * Mathf.Sin(angleVariable) * offsetAmount;
 	}
 
-	private void DoRotateEffect(double delta) {
+	private void DoRotateEffect(double delta)
+	{
 		const float rotationSpeed = 0.04f;
-		Rotate(Vector3.Up, Mathf.DegToRad(angleVariable * (float) delta * rotationSpeed));
+		rotationAngle += rotationSpeed * (float)delta;
+		float radians = Mathf.DegToRad(rotationAngle);
+		Rotate(Vector3.Up, radians);
 	}
 }
