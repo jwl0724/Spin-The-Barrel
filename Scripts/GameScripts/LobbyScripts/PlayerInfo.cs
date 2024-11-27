648790d533ae6bc2b502a1eccdd48dfd287b1f56
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-26 23:38:09 -0800

    add random generation of name

--- Scripts/GameScripts/LobbyScripts/PlayerInfo.cs
+++ Scripts/GameScripts/LobbyScripts/PlayerInfo.cs
@@ -1,6 +1,77 @@
+using System;
+
 public class PlayerInfo {
+	private string[] firstName = {
+		"Goldenrod",
+		"Teal",
+		"Orchid",
+		"Crimson",
+		"ForestGreen",
+		"Gold",
+		"SkyBlue",
+		"Tomato",
+		"LimeGreen",
+		"Maroon",
+		"SeaGreen",
+		"DeepPink",
+		"RoyalBlue",
+		"Salmon",
+		"Turquoise",
+		"Sienna",
+		"Chartreuse",
+		"DarkGray",
+		"Lavender",
+		"OliveDrab",
+		"Firebrick",
+		"PowderBlue",
+		"Coral",
+		"MediumPurple",
+		"Dark Orange"
+	};
+	private string[] lastName = {
+		"Elephant",
+		"Penguin",
+		"Kangaroo",
+		"Dolphin",
+		"Panda",
+		"Tiger",
+		"Giraffe",
+		"Zebra",
+		"Koala",
+		"Cheetah",
+		"Rhinoceros",
+		"Hippopotamus",
+		"Armadillo",
+		"Eagle",
+		"Wolf",
+		"Otter",
+		"Peacock",
+		"Sloth",
+		"Chimpanzee",
+		"Flamingo",
+		"Lemur",
+		"Whale",
+		"Iguana",
+		"Ocelot",
+		"Platypus"
+	};
+
 	// TODO: Add more stuff here later that needs to be tracked in lobby	
-	public bool IsRemote = false;
-	public string Name = "";
+	public bool IsRemote { get; set; }= false;
+	// randomly generated name in MVP
+	public string Name { get; private set; }
 	// probably socket id here when networking is done
+
+	public PlayerInfo() {
+		Random random = new();
+		Name = firstName[random.Next(0, firstName.Length)] + 
+			lastName[random.Next(0, lastName.Length)] + 
+			random.Next(0, 9999).ToString();
+	}
+
+	// call this constructor for remote players
+	public PlayerInfo(string name) {
+		IsRemote = true;
+		Name = name;
+	}
 }
