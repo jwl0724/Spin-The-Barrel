07199299ea7cc169e0697658ae97ce13ce5f3a25
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-27 23:57:03 -0800

    implement clickable for 3d objects

--- Scripts/GeneralScripts/Clickable3D.cs
+++ Scripts/GeneralScripts/Clickable3D.cs
@@ -5,16 +5,30 @@ public partial class Clickable3D : Area3D {
 	// SIGNALS
 	[Signal] public delegate void AreaClickedEventHandler();
 
-	public override void _Ready() {
-		Connect(SignalName.InputEvent, Callable.From(
-			(Node camera, InputEvent inputEvent, Vector3 position, Vector3 normal) =>
-				OnInputEvent(camera, inputEvent, position, normal)
-		));
+	public override void _Input(InputEvent inputEvent) {
+		if (!GetParent().IsProcessingInput()) return;
+		if (inputEvent is not InputEventMouse mouse) return;
+		if(!inputEvent.IsActionPressed(ProjectInputs.INTERACT)) return;
+		bool isClicked = CheckClicked(mouse.Position);
+		if (isClicked) EmitSignal(SignalName.AreaClicked);
 	}
 
-	private void OnInputEvent(Node camera, InputEvent inputEvent, Vector3 position, Vector3 normal) {
-		if(inputEvent.IsActionPressed(ProjectInputs.INTERACT)) {
-			EmitSignal(SignalName.AreaClicked, camera, inputEvent, position,  normal);
-		}
+	private bool CheckClicked(Vector2 mousePosition) {
+		const int raycastLength = 1000;
+		PhysicsDirectSpaceState3D world = GetWorld3D().DirectSpaceState;
+		Camera3D currentCam = GetViewport().GetCamera3D();
+		Vector3 raycastStart = currentCam.ProjectRayOrigin(mousePosition);
+		Vector3 raycastEnd = currentCam.ProjectPosition(mousePosition, raycastLength);
+        PhysicsRayQueryParameters3D query = new() {
+            CollisionMask = 1,
+            CollideWithAreas = true,
+            From = raycastStart,
+            To = raycastEnd
+        };
+		const string dictKey = "collider";
+        Godot.Collections.Dictionary intersect = world.IntersectRay(query);
+		if (intersect.Count == 0) return false;
+		Node collider = intersect[dictKey].As<Node>();
+		return collider.GetInstanceId() == GetInstanceId();
 	}
 }
