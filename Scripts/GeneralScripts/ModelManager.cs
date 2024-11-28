4e0ab6602fa8d1424ad4991fd6847c66fe57ddd2
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-27 23:58:01 -0800

    fix models not loading via resource loader

--- Scripts/GeneralScripts/ModelManager.cs
+++ Scripts/GeneralScripts/ModelManager.cs
@@ -1,19 +1,18 @@
 using Godot;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 public abstract partial class ModelManager : Node3D {
-	protected readonly static Godot.Collections.Array<PackedScene> modelCollection = new();
-	private static readonly string modelDirectoryPath = "res://Scenes/PlayerModels";
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() {
+	protected readonly static Godot.Collections.Array<PackedScene> modelCollection;
+	private static readonly string modelDirectoryPath = "res://Scenes/PlayerModels/";
+	static ModelManager() {
+		modelCollection = new();
 		DirAccess modelDirectory = DirAccess.Open(modelDirectoryPath);
 		if (modelDirectory == null) GD.PushError("Model directory configuration incorrect");
-		modelDirectory.ListDirBegin();
-		string fileName = modelDirectory.GetNext();
-		while(fileName != "") {
-			if (modelDirectory.CurrentIsDir()) GD.PushError("Model directory should not have any directories inside it");
-			else modelCollection.Add(ResourceLoader.Load<PackedScene>(modelDirectoryPath + fileName));
+		var allModels = modelDirectory.GetFiles();
+		foreach(string fileName in allModels) {
+			PackedScene modelScene = ResourceLoader.Load<PackedScene>(modelDirectoryPath + fileName);
+			if (modelScene != null) modelCollection.Add(modelScene);
 		}
 	}
 	
