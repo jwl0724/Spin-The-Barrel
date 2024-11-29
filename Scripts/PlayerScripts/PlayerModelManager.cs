d7ad20c0073c690050c1cf58f9b7e985551e21b5
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-29 01:04:23 -0800

    delete unused _Process

--- Scripts/PlayerScripts/PlayerModelManager.cs
+++ Scripts/PlayerScripts/PlayerModelManager.cs
@@ -4,14 +4,9 @@ public partial class PlayerModelManager : ModelManager {
 	private Player player;
 	private Node3D model = null;
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		player = Owner as Player;
 		player.Connect(Player.SignalName.PlayerReset, Callable.From(() => OnPlayerReset()));
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
 	}
 
 	// Call when multiplayer is implemented, and sync the model with the server, for RemotePlayer
