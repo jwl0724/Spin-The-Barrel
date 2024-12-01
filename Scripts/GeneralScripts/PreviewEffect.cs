8eee19f90a4df954356bb89b8b82ecaeadf99c92
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-12-01 06:46:58 -0800

    move variables to export values

--- Scripts/GeneralScripts/PreviewEffect.cs
+++ Scripts/GeneralScripts/PreviewEffect.cs
@@ -3,27 +3,29 @@ using System;
 
 public partial class PreviewEffect : Node3D
 {
+	[Export] private float rotationSpeed = 40f;
+	[Export] private float hoverSpeed = 8;
+	[Export] private float hoverOffsetAmount = 0.005f;
+	private Vector3 basePosition;
 	private float angleVariable = 0;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		basePosition = Position;
 		DoHoverEffect(delta);
 		DoRotateEffect(delta);
 	}
 
 	private void DoHoverEffect(double delta)
 	{
-		const int offsetSpeed = 8;
-		float offsetAmount = 0.005f;
 		if (angleVariable >= Mathf.Pi * 2) angleVariable = 0;
-		angleVariable += (float)delta * offsetSpeed;
-		Position += Vector3.Up * Mathf.Sin(angleVariable) * offsetAmount;
+		angleVariable += (float) delta * hoverSpeed;
+		Position = Vector3.Up * Mathf.Sin(angleVariable) * hoverOffsetAmount + basePosition;
 	}
 
 	private void DoRotateEffect(double delta)
 	{
-		const float rotationSpeed = 20f;
 		float radians = Mathf.DegToRad(rotationSpeed * (float)delta);
 		Rotate(Vector3.Up, radians);
 	}
