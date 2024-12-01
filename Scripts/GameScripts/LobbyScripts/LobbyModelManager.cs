b8296ca02f6feb2bbb35a9d24a085f4b04ccac07
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-12-01 07:24:15 -0800

    add model name to selector

--- Scripts/GameScripts/LobbyScripts/LobbyModelManager.cs
+++ Scripts/GameScripts/LobbyScripts/LobbyModelManager.cs
@@ -3,13 +3,18 @@ using System;
 using System.Linq;
 
 public partial class LobbyModelManager : ModelManager {
+	private static readonly string randomName = "Random";
 	private static readonly string dumpNodeName = "Models";
+	private static readonly string nameDisplayNodeName = "ModelName";
 	private int modelIndex;
 	private Node3D dumpNode;
+	private Node3D nameNode;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		dumpNode = GetNode<Node3D>(dumpNodeName);
+		nameNode = Owner.GetNode<Node3D>(nameDisplayNodeName);
+		nameNode.Visible = false;
 		foreach(PackedScene model in modelCollection) {
 			Node3D modelNode = model.Instantiate<Node3D>();
 			modelNode.Visible = false;
@@ -21,12 +26,14 @@ public partial class LobbyModelManager : ModelManager {
 		(dumpNode.GetChild(modelIndex) as Node3D).Visible = false;
 		if (++modelIndex == modelCollection.Count + 1) modelIndex = 0;
 		(dumpNode.GetChild(modelIndex) as Node3D).Visible = true;
+		SetNameText();
 	}
 
 	public void PreviousModel() {
 		(dumpNode.GetChild(modelIndex) as Node3D).Visible = false;
 		if (--modelIndex < 0) modelIndex = modelCollection.Count;
 		(dumpNode.GetChild(modelIndex) as Node3D).Visible = true;
+		SetNameText();
 	}
 
 	public int GetSelected() {
@@ -38,10 +45,13 @@ public partial class LobbyModelManager : ModelManager {
 		modelIndex = (int) (GD.Randi() % (modelCollection.Count + 1));
 		(dumpNode.GetChild(modelIndex) as Node3D).Visible = true;
 		ShowSelected(modelIndex);
+		nameNode.Visible = true;
+		SetNameText();
 	}
 
 	public void EndSelector() {
 		(dumpNode.GetChild(modelIndex) as Node3D).Visible = false;
+		nameNode.Visible = false;
 	}
 
 	private void ShowSelected(int index) {
@@ -49,5 +59,11 @@ public partial class LobbyModelManager : ModelManager {
 			if (model == dumpNode.GetChild(index)) model.Visible = true;
 			else model.Visible = false;
 		}
+	}
+
+	private void SetNameText() {
+		TextMesh text = nameNode.GetChild<MeshInstance3D>(0).Mesh as TextMesh;
+		if (modelIndex == 0) text.Text = randomName;
+		else text.Text = GetModelName(modelIndex - 1);
 	}
 }
