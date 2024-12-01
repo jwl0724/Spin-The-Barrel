fa8b4050073234991650f43229e8303ce8873a87
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-11-30 20:20:23 -0800

    add dictionary definition

--- Scripts/ItemScripts/ItemManager.cs
+++ Scripts/ItemScripts/ItemManager.cs
@@ -1,52 +1,51 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-
-
-// PLANNED ITEMS
-
-// HEALING - HEAL BY 1 -> BROCCOLI
-
-// GAMBLE HEALING - 50% DAMAGE 1, 25% HEAL 1, 25% FULL HEAL -> LAB BOTTLE
-
-// DAMAGE BOOST - x2 NEXT BULLET -> COFFEE
-
-// DAMAGE NULLIFIER - BLOCK BULLET -> WOOD SHIELD
-
-// TURN SKIP - SKIP TURN -> ICE CREAM
-
-// TURN CHOOSE - CHOOSE SOMEONE TO GO NEXT -> FOAM FINGER
-
-// OFFENSE - ALLOW PLAYER TO SHOOT SOMEONE ELSE -> SLIME
-
-// REROLL - ALLOW PLAYER TO RE-POSITION GUN -> DICE
-
-// FLIP ORDER - GO BACKWARDS IN TURN -> RUBIKS CUBE
-
-// FORTUNTE - ROLL CHAMBER TO GUARANTEED SAFE POSITION -> MAP
+using System.Text.RegularExpressions;
 
 public partial class ItemManager : Node3D, IInteractableEntity {
 	// Model collections data
-	private static readonly string ITEM_SCRIPT_PATH = "res://Scripts/ItemScripts/Items";
-	[Export] private static readonly Godot.Collections.Array<Node3D> itemModels;
+	private static readonly string ITEM_SCRIPT_PATH = "res://Scripts/ItemScripts/Items/";
+	[Export] private static readonly Godot.Collections.Array<PackedScene> itemModels;
 
 	// Hard coding the values of their respective models
 	private static readonly Dictionary<string, string> descriptionKey = new() { // model name key, model description value
-		
+		{"Broccoli", "Restores 1 health"},
+		{"Chemical", "50% Chance to take damage, 50% chance to heal"},
+		{"Coffee", "Double the damage of the next dart"},
+		{"Shield", "Blocks damage for this turn"},
+		{"Ice Cream", "Skips this turn"},
+		{"Finger", "Choose who goes next"},
+		{"Slime", "Shoot someone else instead"},
+		{"Dice", "Re-randomize chamber position"},
+		{"Compass", "Reverse the turn order for this round"},
+		{"Map", "Puts chamber position to safe position, fails if no safe position"}
 	};
 	private static readonly Dictionary<string, Script> scriptKey = new() { // model name key, associated script value, script needs to implement IItem
-
+		{"Broccoli", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "Broccoli.cs")},
+		{"Chemical", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "Chemical.cs")},
+		{"Coffee", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "Coffee.cs")},
+		{"Shield", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "Shield.cs")},
+		{"Ice Cream", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "IceCream.cs")},
+		{"Finger", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "Finger.cs")},
+		{"Slime", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "Slime.cs")},
+		{"Dice", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "Dice.cs")},
+		{"Compass", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "Compass.cs")},
+		{"Map", ResourceLoader.Load<Script>(ITEM_SCRIPT_PATH + "Map.cs")}
 	};
 
+	private static readonly string USE_ANIMATION = "Use";
 	private static readonly string MODEL_NODE_NAME = "Model";
 	private static readonly string SCRIPT_PLACEMENT_NODE_NAME = "ItemEffect";
+	private static readonly string ANIMATION_PLAYER_NODE_NAME = "Animator";
 
 	private GameDriver gameDriver;
-	private Player holder;
-	private string itemName;
-	private string itemDescription;
+	private AnimationPlayer animator;
 	private Node scriptPlacementNode;
+	private Player holder;
 	private Node3D model;
+	private string itemDescription;
+	private string itemName;
 
 	[Signal] public delegate void ItemUsedEventHandler();
 	[Signal] public delegate void ItemSpawnedEventHandler();
@@ -55,6 +54,7 @@ public partial class ItemManager : Node3D, IInteractableEntity {
 	public override void _Ready() {
 		gameDriver = GameDriver.Instance;
 		scriptPlacementNode = GetNode(SCRIPT_PLACEMENT_NODE_NAME);
+		animator = GetNode<AnimationPlayer>(ANIMATION_PLAYER_NODE_NAME);
 		PrepareItem();
 		EmitSignal(SignalName.ItemSpawned);
 	}
@@ -69,10 +69,14 @@ public partial class ItemManager : Node3D, IInteractableEntity {
 
     public void Interact() {
 		EmitSignal(SignalName.ItemUsed);
-		(scriptPlacementNode as IItem).Use(gameDriver, holder);
+		animator.Play(USE_ANIMATION);
+		animator.Connect(AnimationPlayer.SignalName.AnimationFinished, Callable.From((StringName name) => {
+			if (name == USE_ANIMATION) DestroyItem();
+			(scriptPlacementNode as IItem).Use(gameDriver, holder);
+		}));
     }
 
-	public void DestroyItem() {
+	private void DestroyItem() {
 		Visible = false;
 		EmitSignal(SignalName.ItemDestroyed);
 		
@@ -88,10 +92,10 @@ public partial class ItemManager : Node3D, IInteractableEntity {
 
 	private void PrepareItem() {
 		int randomIndex = (int) (GD.Randi() % itemModels.Count);
-		model = itemModels[randomIndex];
-		itemName = model.Name;
-		itemDescription = descriptionKey[model.Name];
-		scriptPlacementNode.SetScript(scriptKey[model.Name]);
+		model = itemModels[randomIndex].Instantiate<Node3D>();
+		itemName = Regex.Replace(itemModels[randomIndex].ResourceName, "(\\B[A-Z])", " $1"); // puts space in camel case
+		itemDescription = descriptionKey[itemName];
+		scriptPlacementNode.SetScript(scriptKey[itemName]);
 		GetNode<Node3D>(MODEL_NODE_NAME).AddChild(model);
 	}
 }
